36f72c95e675da1cfe8aa73866ffc694864a03a2
JulsOSpel <julsspelman@gmail.com>
2023-04-18 23:48:41 -0700

    Script file to create the SQLite database

---
+++ scripts/makeSQL.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Make folder.
+mkdir -p data
+# Make database and tables.
+sqlite3 data/database.db <<EOF
+CREATE TABLE nmap (
+	ip TEXT,
+	port TEXT,
+	servicename TEXT,
+	state TEXT,
+	scantype TEXT
+);
+CREATE TABLE nmapVersion (
+	ip TEXT,
+	port TEXT,
+	servicename TEXT,
+	state TEXT,
+	servicetype TEXT
+);
+EOF
